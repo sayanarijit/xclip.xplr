ddbcce2a255537ce8e3680575bbe964b49d05979
Arijit Basu <sayanarijit@gmail.com>
2023-01-11 15:47:26 +0530

    Use xplr -m

--- init.lua
+++ init.lua
@@ -7,7 +7,6 @@ local function setup(args)
   args.paste_command = args.paste_command or "xclip-pastefile"
   args.keep_selection = args.keep_selection or false
 
-
   xplr.fn.custom.xclip_copy = function(app)
     local files = {}
     local count = 0
@@ -52,12 +51,12 @@ local function setup(args)
           messages = {
             {
               BashExecSilently = [===[
-              if ]===] .. args.copy_paths_command .. [===[ < "${XPLR_PIPE_RESULT_OUT:?}"; then
-                echo "LogSuccess: Copied path(s) to clipboard" >> "${XPLR_PIPE_MSG_IN:?}"
-              else
-                echo "LogError: Failed to copy path" >> "${XPLR_PIPE_MSG_IN:?}"
-              fi
-              ]===]
+                if ]===] .. args.copy_paths_command .. [===[ < "${XPLR_PIPE_RESULT_OUT:?}"; then
+                  "$XPLR" -m "LogSuccess: %q" "Copied path(s) to clipboard"
+                else
+                  "$XPLR" -m "LogError: %q" "Failed to copy path"
+                fi
+              ]===],
             },
             "PopMode",
           },
@@ -67,40 +66,40 @@ local function setup(args)
           messages = {
             {
               BashExecSilently = [===[
-              if ]===] .. args.copy_paths_command .. [===[ <<< "${PWD:?}"; then
-                echo "LogSuccess: Copied $PWD to clipboard" >> "${XPLR_PIPE_MSG_IN:?}"
-              else
-                echo "LogError: Failed to copy $PWD" >> "${XPLR_PIPE_MSG_IN:?}"
-              fi
-              ]===]
+                if ]===] .. args.copy_paths_command .. [===[ <<< "${PWD:?}"; then
+                  "$XPLR" -m "LogSuccess: %q" "Copied $PWD to clipboard"
+                else
+                  "$XPLR" -m "LogError: %q" "Failed to copy $PWD"
+                fi
+              ]===],
             },
             "PopMode",
           },
         },
-        esc = {
-          help = "cancel",
-          messages = { "PopMode" },
-        },
-        ["ctrl-c"] = {
-          help = "terminate",
-          messages = { "Terminate" },
-        }
-      }
-    }
+      },
+    },
   }
 
   if not args.keep_selection then
-    table.insert(xplr.config.modes.custom.xclip_copy.key_bindings.on_key.y.messages, "ClearSelection")
-    table.insert(xplr.config.modes.custom.xclip_copy.key_bindings.on_key.p.messages, "ClearSelection")
-    table.insert(xplr.config.modes.custom.xclip_copy.key_bindings.on_key.P.messages, "ClearSelection")
+    table.insert(
+      xplr.config.modes.custom.xclip_copy.key_bindings.on_key.y.messages,
+      "ClearSelection"
+    )
+    table.insert(
+      xplr.config.modes.custom.xclip_copy.key_bindings.on_key.p.messages,
+      "ClearSelection"
+    )
+    table.insert(
+      xplr.config.modes.custom.xclip_copy.key_bindings.on_key.P.messages,
+      "ClearSelection"
+    )
   end
-
 
   xplr.config.modes.builtin.default.key_bindings.on_key.y = {
     help = "copy to clipboard",
     messages = {
       "PopMode",
-      { SwitchModeCustom = "xclip_copy" }
+      { SwitchModeCustom = "xclip_copy" },
     },
   }
 
